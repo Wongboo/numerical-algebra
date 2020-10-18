08f307c94199da3e53d75406819bb572b8d2f56f
ustcw0ng <ustcw0ng@mail.ustc.edu.cn>
2020-10-18 19:05:25 +0800

    nothing

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Numerical algebra assignment, just for reference, do not copy blindly.
 ## Happy_matrix_1
 ### main feature
 - Reuse of code. Generating 5 executives with 3 files. Theoretically, it can be done within one file, checkout [dev](https://github.com/Wongboo/numerical-algebra/tree/dev)! By doing so, you can change 2_1 and 3_1 or 2_2 and 3_2 at once
-- Approriate use of template and concept, so that you can change size and type of matrix by only editing T and size
+- Approriate use of template and concept, so that you can change size and type of matrix by only editing T and size in one line
 ### main fallback
 - Does not guarantee exception-safe, I sacrifice it for size of executive. If you want to, add suitable exception such as *underflow error* to the code.
 ## License  
--- happy_matrix_1/include/auxiliary.hpp
+++ happy_matrix_1/include/auxiliary.hpp
@@ -16,9 +16,9 @@ namespace happy_matrix{
 
     template<typename T, bool is_compare = false>
     __forceinline void auxiliary_impl(solving<T> solve, const char* out, const matrix<T>& to_decompose_, const vector<T>& to_solve_){
+        assert(to_decompose_.size() == to_decompose_[0].size() && to_decompose_.size() == to_solve_.size());
         auto to_decompose = to_decompose_;
         auto to_solve = to_solve_;
-
         auto start = std::chrono::high_resolution_clock::now();
 
         solve(to_decompose, to_solve);
@@ -29,20 +29,17 @@ namespace happy_matrix{
 
         for (const auto& item: to_solve)
             std::cout << item << " ";
-        std::cout << '\n';
 
         if constexpr (is_compare) {
             to_solve = to_decompose_ * to_solve;
-            std::cout << out << " before: ";
+            std::cout << '\n' << out << " before: ";
             for (const auto &item: to_solve_)
                 std::cout << item << " ";
-            std::cout << '\n';
-            std::cout << out << " after: ";
+            std::cout << '\n' << out << " after: ";
             for (const auto &item: to_solve)
                 std::cout << item << " ";
-            std::cout << '\n';
         }
-        std::cout << '\n';
+        std::cout << '\n' << '\n';
     }
 
 #define auxiliary(a, b, c) auxiliary_impl(a, #a, b, c)
